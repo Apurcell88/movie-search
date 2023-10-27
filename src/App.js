f1e16a2189ff203eeb25fa8d8357ea97f36674a2
Adam Purcell <apurcell88@gmail.com>
2023-10-27 13:58:09 -0500

    Set up useEffect in Nav.js to fetch movie data.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Nav from './components/Nav';
+import Movies from './api/movieDbApi';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  // STATE
+  const [movies, setMovies] = useState([]);
+
   return (
     <div className="App">
-      <Nav />
+      <Nav
+        movies={movies}
+        setMovies={setMovies}
+      />
+      {/* <Movies /> */}
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,26 @@
 import Stack from 'react-bootstrap/Stack';
+import { useEffect } from 'react';
 
-const Nav = () => {
+const Nav = ({ movies, setMovies }) => {
+  useEffect((props) => {
+    const fetchMovies = async () => {
+      try {
+        const apiKey = '51199ad127900c0adc25977cf075af18';
+
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+
+        const data = await res.json();
+
+        setMovies(data.results);
+        console.log(movies);
+      } catch (error) {
+        console.error('Error fetching movie data', error);
+      }
+    };
+
+    fetchMovies();
+  });
+
   return (
     <Stack className='nav-stack' direction="horizontal" gap={1}>
       <div className='p-2'>
